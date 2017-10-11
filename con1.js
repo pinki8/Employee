806fa1b5f15600845f126375062a0f0c93f680f1
pinki8 <pinkibhawariya190594@gmail.com>
2017-10-11 13:57:38 +0530

    js file

--- con1.js
+++ con1.js
@@ -4,9 +4,9 @@ MongoClient.connect("mongodb://localhost:27017/info", function (err, db) {
     
     db.collection('emp', function (err, collection) {
         
-        collection.insert({ id: 1, firstName: 'Priya', lastName: 'Bharti', age: 21 ,email: 'www.priyabharti95@gmail.com',phone: '7020012721',address:'Jaipur' });
-             collection.insert({ id: 2, firstName: 'Anamika', lastName: 'Sharma', age: 22 ,email: 'anamikasharma190594@gmail.com',phone: '8390136948',address:'Bihar' });
-            collection.insert({ id: 3, firstName: 'Nutan', lastName: 'Kuchhadia', age: 22 ,email: 'snowhite09@gmail.co ',phone: '7767982936',address:'Gujrat' });
+        collection.insert({ id: 2, firstName: 'Priya', lastName: 'Bharti', age: 21 ,email: 'www.priyabharti95@gmail.com',phone: '7020012721',address:'Jaipur' });
+             collection.insert({ id: 3, firstName: 'Anamika', lastName: 'Sharma', age: 22 ,email: 'anamikasharma190594@gmail.com',phone: '8390136948',address:'Bihar' });
+            collection.insert({ id: 4, firstName: 'Nutan', lastName: 'Kuchhadia', age: 22 ,email: 'snowhite09@gmail.co ',phone: '7767982936',address:'Gujrat' });
         
         
 
@@ -18,3 +18,4 @@ MongoClient.connect("mongodb://localhost:27017/info", function (err, db) {
     });
                 
 });
+
