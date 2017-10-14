e977f623173c1959e9be2139fac3e2ee82bf7a7a
pinki8 <pinkibhawariya190594@gmail.com>
2017-10-14 11:10:49 +0530

    After removing id

--- con1.js
+++ con1.js
@@ -4,9 +4,7 @@ MongoClient.connect("mongodb://localhost:27017/info", function (err, db) {
     
     db.collection('emp', function (err, collection) {
         
-        collection.insert({ id: 2, firstName: 'Priya', lastName: 'Bharti', age: 21 ,email: 'www.priyabharti95@gmail.com',phone: '7020012721',address:'Jaipur' });
-             collection.insert({ id: 3, firstName: 'Anamika', lastName: 'Sharma', age: 22 ,email: 'anamikasharma190594@gmail.com',phone: '8390136948',address:'Bihar' });
-            collection.insert({ id: 4, firstName: 'Nutan', lastName: 'Kuchhadia', age: 22 ,email: 'snowhite09@gmail.co ',phone: '7767982936',address:'Gujrat' });
+              collection.insert({firstName: 'Annu', lastName: 'Yadav', age: 22 ,email: 'annuyads09@gmail.co ',phone: '7767983560',address:'Haryana' });
         
         
 
