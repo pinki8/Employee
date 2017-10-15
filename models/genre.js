d29abf7c623e34e87cbb50e683f4ea43ce585592
pinki8 <pinkibhawariya190594@gmail.com>
2017-10-15 19:39:23 +0530

    New file

---
+++ models/genre.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose');
+var genreSchema=mongoose.Schema({
+	firstName:{
+		type :String,
+		required :true
+			},
+	lastName:{
+		type :String,
+		required :true},
+	age:{
+		type :Number,
+		required :true
+			},
+	email:{
+		type :String,
+		required :true
+			},
+	phone:{
+		type :String,
+		required :true
+			},
+	address:{
+		type :String,
+		required :true
+			}
+});	
+var Genre=module.exports=mongoose.model('Genre',genreSchema);					
+module.exports.getGenres=function(callback,limit)
+{
+	Genre.find(callback).limit(5);
+}
