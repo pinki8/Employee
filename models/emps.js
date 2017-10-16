90f2dbb24e3fd5aa40fcf94447a6a2b8d5351eeb
pinki8 <pinkibhawariya190594@gmail.com>
2017-10-16 18:31:04 +0530

    Changes...

--- models/emps.js
+++ models/emps.js
@@ -1,31 +1,9 @@
 var mongoose = require('mongoose');
 var empsSchema=mongoose.Schema({
-	firstName:{
-		type :String,
-		required :true
-			},
-	lastName:{
-		type :String,
-		required :true},
-	age:{
-		type :Number,
-		required :true
-			},
-	email:{
-		type :String,
-		required :true
-			},
-	phone:{
-		type :String,
-		required :true
-			},
-	address:{
-		type :String,
-		required :true
-			}
+	
 });	
-var emps1=module.exports=mongoose.model('emps1',empsSchema);					
-module.exports.getemp=function(callback,limit)
+var Emps=module.exports=mongoose.model('Emps',empsSchema);					
+module.exports.getEmps=function(callback,limit)
 {
-	emps1.find(callback).limit(limit);
+	Emps.find(callback).limit(5);
 }
