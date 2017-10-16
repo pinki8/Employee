119f2c99f4e20f1ee775671dc385ff2e9779c1ec
pinki8 <pinkibhawariya190594@gmail.com>
2017-10-16 12:50:59 +0530

    post api

--- con1.js
+++ con1.js
@@ -3,6 +3,7 @@ var app=express();
 var bodyParser=require('body-parser');
 var mongoose = require('mongoose');
 
+app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost/info');
 var db=mongoose.connection;
 Genre=require('./models/genre');
@@ -21,5 +22,16 @@ app.get('/api/genres',function(req,res)
 	});
 
 });
+app.post('/api/genres',function(req,res)
+{			var genre=req.body;
+			//console.log(req.body.firstName);});
+			Genre.addGenre(genre,function(err,genre){
+ 			if(err){
+ 				throw err;
+ 			}
+		res.json(genre);
+	});
+});
+
 app.listen(3000);
 console.log('Running on port 3000....');
